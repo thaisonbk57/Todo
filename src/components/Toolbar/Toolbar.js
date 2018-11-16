4f5b2c623255701bc7f7a1df92510e3d89ecf0d7
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-16 16:33:04 +0100

    added Toolbar

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//////////////////////////
+
+import "./Toolbar.scss";
+
+///////////////////////////
+
+import Logo from "./../UI/Logo/Logo";
+
+const Toolbar = props => {
+  return (
+    <div className="Toolbar">
+      <Logo />
+    </div>
+  );
+};
+
+export default Toolbar;
