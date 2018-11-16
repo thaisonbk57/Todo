751b0a7bc77b9fc2e646991e662ad6e661bc65ae
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-16 16:43:35 +0100

    create DrawerToggle cmp for Toolbar cmp

---
+++ src/components/Toolbar/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DrawerToggle = () => {
+  return (
+    <div className="DrawerToggle">
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default DrawerToggle;
