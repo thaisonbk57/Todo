799060ae905fdeca95424df6153835bc8c82e3b8
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-17 03:09:05 +0100

    created SideDrawer cmp

---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+//////////////////////////
+
+import "./SideDrawer.scss";
+
+export default class SideDrawer extends Component {
+  render() {
+    return <div className="SideDrawer">Side</div>;
+  }
+}
