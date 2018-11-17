87c19e0ac46049e478f758296799a71d5a8d42b1
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-17 03:09:22 +0100

    added SideDrawer cmp to Layout cmp

--- src/HOC/Layout/Layout.js
+++ src/HOC/Layout/Layout.js
@@ -7,6 +7,7 @@ import "./Layout.scss";
 ////////////////////
 
 import Toolbar from "./../../components/Toolbar/Toolbar";
+import SideDrawer from "./../../components/SideDrawer/SideDrawer";
 
 /////////////////////
 
@@ -14,6 +15,7 @@ export default class Layout extends Component {
   render() {
     return (
       <div className="Layout">
+        <SideDrawer />
         <Toolbar />
       </div>
     );
