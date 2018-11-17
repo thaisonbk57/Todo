f2dfe187fb108afa567c0ff2d026e0e041e729a6
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-17 23:58:19 +0100

    make AddTodoForm be a controlled cmp

--- src/components/AddTodoForm/AddTodoForm.js
+++ src/components/AddTodoForm/AddTodoForm.js
@@ -7,11 +7,33 @@ import "./AddTodoForm.scss";
 /////////////////////////////////
 
 export default class AddTodoForm extends Component {
+  state = {
+    task: ""
+  };
+
+  onInputChanged = e => {
+    const target = e.target;
+    let value = target.value;
+
+    this.setState(() => {
+      return {
+        task: value
+      };
+    });
+  };
+
   render() {
     return (
       <div className="AddTodoForm">
         <div className="form-group">
-          <input type="text" className="form-control" />
+          <input
+            value={this.state.task}
+            type="text"
+            className="form-control"
+            onChange={e => {
+              this.onInputChanged(e);
+            }}
+          />
           <button />
         </div>
       </div>
