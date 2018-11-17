07dda9720cf6a4ba3e1acd5800bbb1217a2c6a6e
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-18 00:11:10 +0100

    created addTodoHandler method

--- src/containers/TodoBuilder/TodoBuilder.js
+++ src/containers/TodoBuilder/TodoBuilder.js
@@ -9,10 +9,29 @@ import "./TodoBuilder.scss";
 import AddTodoForm from "./../../components/AddTodoForm/AddTodoForm";
 
 export default class TodoBuilder extends Component {
+  state = {
+    todos: [
+      { id: 1, task: "do homework", completed: false },
+      { id: 2, task: "go shopping", completed: true },
+      { id: 3, task: "appointment with dentist", completed: false },
+      { id: 4, task: "walk the dog", completed: false }
+    ]
+  };
+
+  addTodoHandler = todo => {
+    if (!!todo) {
+      this.setState(state => {
+        return {
+          todos: [...state.todos, todo]
+        };
+      });
+    } else return;
+  };
+
   render() {
     return (
       <div className="TodoBuilder">
-        <AddTodoForm />
+        <AddTodoForm addTodoHandler={this.addTodoHandler} />
       </div>
     );
   }
