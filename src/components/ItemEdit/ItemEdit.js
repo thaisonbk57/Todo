7be22f2c65727d278c039910204ab033d390e8ab
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-19 00:59:25 +0100

    created and styled ItemEdit cmp

---
+++ src/components/ItemEdit/ItemEdit.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+///////////////////////
+
+import "./ItemEdit.scss";
+
+///////////////////////
+
+const ItemEdit = props => {
+  return (
+    <div className="ItemEdit">
+      <input type="text" className="form-control" />
+      <button />
+    </div>
+  );
+};
+
+export default ItemEdit;
