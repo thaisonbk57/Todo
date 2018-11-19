ea86bb1ea0feec6d395d9b3c55487b6580293991
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-19 10:17:53 +0100

    this section is under construction

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -8,7 +8,16 @@ export default class SideDrawer extends Component {
   render() {
     return (
       <div className={this.props.show ? "SideDrawer Open" : "SideDrawer"}>
-        Side
+        <h1
+          style={{
+            color: "white",
+            textAlign: "center",
+            marginTop: 50,
+            fontWeight: 300
+          }}
+        >
+          This section is under construction.
+        </h1>
       </div>
     );
   }
