8ae98b5c26fa85b341883dabbfe22ede36247bed
Thaison Dang <thaisondang.bk57@gmail.com>
2018-12-03 15:48:19 +0100

    added Routes and 404 Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Route, Switch } from "react-router-dom";
 //////////////////////////////////////////
 
 import "./App.scss";
@@ -8,6 +8,7 @@ import "./App.scss";
 
 import Layout from "./HOC/Layout/Layout";
 import TodoBuilder from "./containers/TodoBuilder/TodoBuilder";
+import Page404 from "./pages/404/404";
 
 //////////////////////////////////////////
 
@@ -16,7 +17,10 @@ class App extends Component {
     return (
       <div className="App">
         <Layout>
-          <TodoBuilder />
+          <Switch>
+            <Route path="/" exact component={TodoBuilder} />
+            <Route component={Page404} />
+          </Switch>
         </Layout>
       </div>
     );
---
+++ src/pages/404/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page404 = props => {
+  return (
+    <div className="container">
+      <h1 className="text-center text-danger display-2">Page not found!</h1>
+    </div>
+  );
+};
+
+export default Page404;
